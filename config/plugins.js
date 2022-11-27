14add206daab85af0daa7b05301c6a57ec4d7123
ricardoTellezA <lian.redi@hotmail.com>
2022-11-27 12:58:05 -0600

    Strapi

--- config/plugins.js
+++ config/plugins.js
@@ -2,11 +2,22 @@ module.exports = ({ env }) => ({
   // ...
   upload: {
     config: {
-      provider: 'cloudinary',
+      provider: "cloudinary",
       providerOptions: {
-        cloud_name: env('CLOUDINARY_NAME'),
-        api_key: env('CLOUDINARY_KEY'),
-        api_secret: env('CLOUDINARY_SECRET'),
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      "vercel-deploy": {
+        enabled: true,
+        config: {
+          deployHook:
+            env("VERCEL_DEPLOY_PLUGIN_HOOK"),
+          apiToken: env("VERCEL_DEPLOY_PLUGIN_API_TOKEN"),
+          appFilter: env("VERCEL_DEPLOY_PLUGIN_APP_FILTER"),
+          teamFilter: "deploy",
+          roles:  [ "strapi-super-admin" ],
+        },
       },
       actionOptions: {
         upload: {},
